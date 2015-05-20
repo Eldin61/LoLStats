05a8d4fd2002bb2cd961a151b867b66e01c6f95e
NiekGuijt <0897847@hr.nl>
2015-05-20 13:14:56 +0200

    hoi eldin

--- app/src/main/java/lolstats/eldin/com/lolstats/currentMatch.java
+++ app/src/main/java/lolstats/eldin/com/lolstats/currentMatch.java
@@ -1,34 +1,104 @@
 package lolstats.eldin.com.lolstats;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.AsyncTask;
+
+import com.robrua.orianna.api.core.RiotAPI;
+import com.robrua.orianna.type.core.common.Region;
+import com.robrua.orianna.type.core.stats.PlayerStatsSummaryType;
+import com.robrua.orianna.type.core.summoner.Summoner;
+import com.robrua.orianna.type.exception.APIException;
 
 /**
  * Created by Eldin on 18-5-2015.
  */
 public class currentMatch extends AsyncTask<Void, Void, Void>{
+    //static String sName;
+    static String sName;
+    String userName;
+    String currentGame;
+    public boolean sFound;
+
+    ProgressDialog p;
+
+    Activity mActivity;
+
 
     Activity o;
-    public currentMatch(Activity a){
-        o = a;
+    public currentMatch(Activity activity){
+        mActivity = activity;
     }
 
     @Override
     protected void onPreExecute(){
 
+        p = ProgressDialog.show(mActivity, "Loading", "Retrieving and calculating data..", true);
+
     }
+
+
 
     @Override
     protected Void doInBackground(Void... params) {
+        try {
+            RiotAPI.setMirror(Region.EUW);
+            RiotAPI.setRegion(Region.EUW);
+            RiotAPI.setAPIKey("ebe43318-cee4-4d2a-bf19-1c195a32aa93");
+            Summoner summoner = RiotAPI.getSummonerByName(sName);
+            String Player = summoner.getCurrentGame().toString();
+            userName = summoner.getName();
+
+            Player = Player.replaceAll("Participant", "").replaceAll("\\[", "").replaceAll("\\]", "").replaceAll("\\(", "").replaceAll("\\)", "").replaceAll("CurrentGameInfo", "");
+
+            String[] parts = Player.split(",");
+            String Summoner1 = parts[0];
+            String Summoner2 = parts[1];
+            String Summoner3 = parts[2];
+            String Summoner4 = parts[3];
+            String Summoner5 = parts[4];
+            String Summoner6 = parts[5];
+            String Summoner7 = parts[6];
+            String Summoner8 = parts[7];
+            String Summoner9 = parts[8];
+            String Summoner10 = parts[9];
+
+            currentGame = ("\n" + "Blue team:" + "\n" + Summoner1 + "\n" + Summoner2 + "\n" + Summoner3 + "\n" + Summoner4 + "\n" + Summoner5 + "\n" + "Red team:" + "\n" + Summoner6 + "\n" + Summoner7 + "\n" + Summoner8 + "\n" + Summoner9 + "\n" + Summoner10);
+
+            sFound = true;
+        } catch (APIException e)
+        {
+            sFound = false;
+        }
         return null;
     }
 
     @Override
     protected void onPostExecute(Void unused){
-        super.onPostExecute(unused);
-        Intent intent = new Intent(o, currentMatchPage.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        o.startActivity(intent);
+        p.dismiss();
+        if (sFound){
+            super.onPostExecute(unused);
+            Intent intent = new Intent(mActivity, currentMatchPage.class);
+            intent.putExtra("name", userName);
+            intent.putExtra("currentGame", currentGame);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            mActivity.startActivity(intent);
+        } else {
+            AlertDialog.Builder builder1 = new AlertDialog.Builder(mActivity);
+            builder1.setMessage("Summoner not in game.");
+            builder1.setCancelable(true);
+            builder1.setPositiveButton("Ok",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            dialog.cancel();
+                        }
+                    });
+
+            final AlertDialog alert11 = builder1.create();
+            alert11.show();
+        }
     }
 }
--- app/src/main/java/lolstats/eldin/com/lolstats/currentMatchPage.java
+++ app/src/main/java/lolstats/eldin/com/lolstats/currentMatchPage.java
@@ -2,6 +2,7 @@ package lolstats.eldin.com.lolstats;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.widget.TextView;
 
 /**
  * Created by Eldin on 18-5-2015.
@@ -11,5 +12,20 @@ public class currentMatchPage extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.currentmatch);
+
+        String userName = "";
+        String currentGame = "";
+
+        Bundle extras = getIntent().getExtras();
+        if(extras != null){
+            userName = extras.getString("name");
+            currentGame = extras.getString("currentGame");
+
+        }
+        TextView tvUserName = (TextView) findViewById(R.id.Username);
+        tvUserName.setText(userName);
+
+        TextView tvCurrentGame = (TextView) findViewById(R.id.CurrentGame);
+        tvCurrentGame.setText("Level: " + currentGame);
     }
 }
